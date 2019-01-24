dec6b5ed9aeb0f65c31b6014e471e55dd8137f96
DevExpressExampleBot <fakeEmail>
2019-01-24 10:14:20 +0300

    Source auto update [skip ci]

---
+++ VB/DXWebApplication1/Designer.aspx.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.DataAccess.Sql
+Imports SqlDataSource = DevExpress.DataAccess.Sql.SqlDataSource
+
+Namespace DXWebApplication1
+    Partial Public Class Designer
+        Inherits System.Web.UI.Page
+
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+            If Not IsPostBack Then
+                BindToData()
+            End If
+        End Sub
+
+        Private Sub BindToData()
+            ' Create a SQL data source with the specified connection string.
+            Dim ds As New SqlDataSource("NWindConnectionString")
+
+            ' Create a SQL query to access the Products data table.
+            Dim query As SelectQuery = SelectQueryFluentBuilder.AddTable("Products").SelectAllColumnsFromTable().Build("Products")
+            ds.Queries.Add(query)
+            ds.RebuildResultSchema()
+
+            ' Add the created data source to the list of default data sources. 
+            ASPxReportDesigner1.DataSources.Add("Northwind", ds)
+            ASPxReportDesigner1.OpenReport("Report")
+        End Sub
+    End Class
+End Namespace
---
+++ VB/DXWebApplication1/Global.asax.vb
@@ -0,0 +1,20 @@
+﻿Imports System
+Imports System.IO
+Imports System.Web
+Imports DevExpress.XtraReports.Web
+
+Namespace DXWebApplication1
+    Public Class Global_asax
+        Inherits System.Web.HttpApplication
+
+        Private Sub Application_Start(ByVal sender As Object, ByVal e As EventArgs)
+            System.Web.Routing.RouteTable.Routes.MapPageRoute("defaultRoute", "", "~/Default.aspx")
+            DevExpress.XtraReports.Configuration.Settings.Default.UserDesignerOptions.DataBindingMode = DevExpress.XtraReports.UI.DataBindingMode.Expressions
+            DevExpress.XtraReports.Web.WebDocumentViewer.Native.WebDocumentViewerBootstrapper.SessionState = System.Web.SessionState.SessionStateBehavior.Default
+            DevExpress.XtraReports.Web.QueryBuilder.Native.QueryBuilderBootstrapper.SessionState = System.Web.SessionState.SessionStateBehavior.Default
+            DevExpress.XtraReports.Web.ReportDesigner.Native.ReportDesignerBootstrapper.SessionState = System.Web.SessionState.SessionStateBehavior.Default
+            DevExpress.XtraReports.Web.Extensions.ReportStorageWebExtension.RegisterExtensionGlobal(New ReportStorageWebExtension1(Path.Combine(HttpRuntime.AppDomainAppPath, "Reports")))
+            ASPxReportDesigner.StaticInitialize()
+        End Sub
+    End Class
+End Namespace
