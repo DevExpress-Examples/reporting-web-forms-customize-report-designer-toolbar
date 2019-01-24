faf6f3792005c07142513691216d7e5f515d9335
BarkovskiyMaksim <maksim.barkovskiy@devexpress.com>
2019-01-24 15:01:28 +0300

    fix default routing

--- CS/DXWebApplication1/Global.asax.cs
+++ CS/DXWebApplication1/Global.asax.cs
@@ -6,7 +6,7 @@ using DevExpress.XtraReports.Web;
 namespace DXWebApplication1 {
     public class Global_asax : System.Web.HttpApplication {
         void Application_Start(object sender, EventArgs e) {
-            System.Web.Routing.RouteTable.Routes.MapPageRoute("defaultRoute", "", "~/Default.aspx");
+            System.Web.Routing.RouteTable.Routes.MapPageRoute("defaultRoute", "", "~/Designer.aspx");
             DevExpress.XtraReports.Configuration.Settings.Default.UserDesignerOptions.DataBindingMode = DevExpress.XtraReports.UI.DataBindingMode.Expressions;
             DevExpress.XtraReports.Web.WebDocumentViewer.Native.WebDocumentViewerBootstrapper.SessionState = System.Web.SessionState.SessionStateBehavior.Default;
             DevExpress.XtraReports.Web.QueryBuilder.Native.QueryBuilderBootstrapper.SessionState = System.Web.SessionState.SessionStateBehavior.Default;
