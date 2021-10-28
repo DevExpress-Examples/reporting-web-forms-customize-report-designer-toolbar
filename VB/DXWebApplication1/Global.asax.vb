c871ea5f6cea44ee30d65de778d8675d577bc43f
DevExpressExampleBot <fakeEmail>
2021-10-28 16:41:06 +0300

    autogenerate VB

--- VB/DXWebApplication1/Global.asax.vb
+++ VB/DXWebApplication1/Global.asax.vb
@@ -1,20 +1,21 @@
-﻿Imports System
+Imports System
 Imports System.IO
 Imports System.Web
 Imports DevExpress.XtraReports.Web
 
 Namespace DXWebApplication1
-	Public Class Global_asax
-		Inherits System.Web.HttpApplication
 
-		Private Sub Application_Start(ByVal sender As Object, ByVal e As EventArgs)
-			System.Web.Routing.RouteTable.Routes.MapPageRoute("defaultRoute", "", "~/Designer.aspx")
-			DevExpress.XtraReports.Configuration.Settings.Default.UserDesignerOptions.DataBindingMode = DevExpress.XtraReports.UI.DataBindingMode.Expressions
-			DevExpress.XtraReports.Web.WebDocumentViewer.Native.WebDocumentViewerBootstrapper.SessionState = System.Web.SessionState.SessionStateBehavior.Default
-			DevExpress.XtraReports.Web.QueryBuilder.Native.QueryBuilderBootstrapper.SessionState = System.Web.SessionState.SessionStateBehavior.Default
-			DevExpress.XtraReports.Web.ReportDesigner.Native.ReportDesignerBootstrapper.SessionState = System.Web.SessionState.SessionStateBehavior.Default
-			DevExpress.XtraReports.Web.Extensions.ReportStorageWebExtension.RegisterExtensionGlobal(New ReportStorageWebExtension1(Path.Combine(HttpRuntime.AppDomainAppPath, "Reports")))
-			ASPxReportDesigner.StaticInitialize()
-		End Sub
-	End Class
+    Public Class Global_asax
+        Inherits HttpApplication
+
+        Private Sub Application_Start(ByVal sender As Object, ByVal e As EventArgs)
+            Routing.RouteTable.Routes.MapPageRoute("defaultRoute", "", "~/Designer.aspx")
+            DevExpress.XtraReports.Configuration.Settings.[Default].UserDesignerOptions.DataBindingMode = DevExpress.XtraReports.UI.DataBindingMode.Expressions
+            DevExpress.XtraReports.Web.WebDocumentViewer.Native.WebDocumentViewerBootstrapper.SessionState = SessionState.SessionStateBehavior.Default
+            DevExpress.XtraReports.Web.QueryBuilder.Native.QueryBuilderBootstrapper.SessionState = SessionState.SessionStateBehavior.Default
+            DevExpress.XtraReports.Web.ReportDesigner.Native.ReportDesignerBootstrapper.SessionState = SessionState.SessionStateBehavior.Default
+            DevExpress.XtraReports.Web.Extensions.ReportStorageWebExtension.RegisterExtensionGlobal(New ReportStorageWebExtension1(Path.Combine(HttpRuntime.AppDomainAppPath, "Reports")))
+            ASPxReportDesigner.StaticInitialize()
+        End Sub
+    End Class
 End Namespace
