8323db6aeee594b8498ac5005629f297e3c85866
BrianDX <boris.zaitsev@devexpress.com>
2022-01-18 00:26:02 +0300

    fix vb

--- VB/DXWebApplication1/ReportStorageWebExtension.vb
+++ VB/DXWebApplication1/ReportStorageWebExtension.vb
@@ -52,8 +52,7 @@ Namespace DXWebApplication1
             ' Returns a dictionary of the existing report URLs and display names. 
             ' This method is called when running the Report Designer, 
             ' before the Open Report and Save Report dialogs are shown and after a new report is saved to a storage.
-            Return Directory.GetFiles(reportDirectory, "*" & FileExtension).[Select](New Func(Of String, String)(AddressOf Path.GetFileNameWithoutExtension)).ToDictionary(Of String, String)(Function(x) x)
-                        Return Directory.GetFiles(reportDirectory, "*" & FileExtension).Select(AddressOf Path.GetFileNameWithoutExtension).ToDictionary(Function(x) x)
+            Return Directory.GetFiles(reportDirectory, "*" & FileExtension).Select(AddressOf Path.GetFileNameWithoutExtension).ToDictionary(Function(x) x)
         End Function
 
         Public Overrides Sub SetData(ByVal report As XtraReport, ByVal url As String)
